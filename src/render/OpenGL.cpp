8710a93e3b108d3a031f6cd1db09bbc82a247104
imperishableSecret <liferacer2310@gmail.com>
2026-05-07 00:04:23 +0530

    render/opengl: clip texture draws to damage

--- src/render/OpenGL.cpp
+++ src/render/OpenGL.cpp
@@ -1587,6 +1587,8 @@ void CHyprOpenGLImpl::renderTextureInternal(SP<ITexture> tex, const CBox& box, c
                 damageClip.intersect(data.clipRegion);
         }
 
+        damageClip.intersect(*data.damage);
+
         if (!damageClip.empty()) {
             damageClip.forEachRect([this](const auto& RECT) {
                 scissor(&RECT, g_pHyprRenderer->m_renderData.transformDamage);
